1e72654757eead787916618e79aeb4cb2d695ef8
ZanuZoss <120405478+ZanuZoss@users.noreply.github.com>
2024-06-25 19:03:17 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var i = 0;
+    var txt = 'zanufetch';
+    var speed = 150;
+
+    function typeWriter() {
+        if (i < txt.length) {
+            document.getElementById("zff").innerHTML += txt.charAt(i);
+            i++;
+            setTimeout(typeWriter, speed);
+        }
+    }
+
+    typeWriter();
+
+    setTimeout(function() {
+        document.querySelector('.zanufetch').style.opacity = '1';
+      }, 1400);
+
+      function updateText() {
+        if (window.innerWidth <= 1000) {
+          document.querySelector('.cose').textContent = 'Software Enginner...';
+        } else {
+          document.querySelector('.cose').textContent = 'Software Enginner & Designer';
+        }
+      }
+    
+      updateText();
+      window.addEventListener('resize', updateText);
+});
