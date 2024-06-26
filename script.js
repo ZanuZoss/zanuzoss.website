db16258fb95a6d0f2ea1316f4b844ed69fdecc5b
ZanuZoss <120405478+ZanuZoss@users.noreply.github.com>
2024-06-26 17:36:36 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
+// Designed by ZanuZoss, coded by ZanuZoss
+
 document.addEventListener("DOMContentLoaded", function() {
-    var i = 0;
-    var txt = 'zanufetch';
-    var speed = 150;
+  var i = 0;
+  var txt = 'zanufetch';
+  var speed = 150;
 
-    function typeWriter() {
-        if (i < txt.length) {
-            document.getElementById("zff").innerHTML += txt.charAt(i);
-            i++;
-            setTimeout(typeWriter, speed);
-        }
-    }
-
-    typeWriter();
-
-    setTimeout(function() {
-        document.querySelector('.zanufetch').style.opacity = '1';
-      }, 1400);
-
-      function updateText() {
-        if (window.innerWidth <= 1000) {
-          document.querySelector('.cose').textContent = 'Software Enginner...';
-        } else {
-          document.querySelector('.cose').textContent = 'Software Enginner & Designer';
-        }
+  function typeWriter() {
+      if (i < txt.length) {
+          document.getElementById("zff").innerHTML += txt.charAt(i);
+          i++;
+          setTimeout(typeWriter, speed);
       }
-    
-      updateText();
-      window.addEventListener('resize', updateText);
+  }
+
+  typeWriter();
+
+  setTimeout(function() {
+      document.querySelector('.zanufetch').style.opacity = '1';
+  }, 1400);
+
+  function updateText() {
+      if (window.innerWidth <= 1000) {
+          document.querySelector('.cose').textContent = 'Software Enginner...';
+      } else {
+          document.querySelector('.cose').textContent = 'Software Enginner & Designer';
+      }
+  }
+
+  updateText();
+  window.addEventListener('resize', updateText);
 });
